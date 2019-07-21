170a528fb1440b984f4e3d9afbda6ab3ca5aa66a
Callum McClean <callum.mcclean@ditto.ai>
2019-07-21 13:37:12 +0100

    Add express and initial route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
- const HelloWorld = () => {
-    console.log("Hello World!");
- }
+import * as express from 'express';
+import * as parser from 'body-parser';
 
- HelloWorld();
+const app: express.Application = express();
+app.use(parser.json());
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Shopping Cart Service started on port 3000!');
+});
