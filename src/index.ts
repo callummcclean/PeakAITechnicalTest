c4e6718a154916ba57f50278cabadcf6044ac0f3
Callum McClean <callum.mcclean@ditto.ai>
2019-07-21 14:18:03 +0100

    Add routes to cart functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
 import * as express from 'express';
 import * as parser from 'body-parser';
+import * as uuid from 'uuid';
+
+import Cart from './cart/Cart';
+import Item from './item/Item';
+import ICart from 'cart/ICart';
 
 const app: express.Application = express();
 app.use(parser.json());
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Hello World!',
-  });
+const cart: ICart = new Cart();
+
+// add items to cart
+app.post('/cart/items', (req: express.Request, res: express.Response) => {
+  const item = req.body as Item;
+
+  cart.add(item);
+  const items = cart.getAll();
+
+  res.send(items);
+});
+
+// list all items
+app.get('/cart/items', (req: express.Request, res: express.Response) => {
+  const items = cart.getAll();
+
+  res.send(items);
+});
+
+// remove item from cart
+app.delete('/cart/items/:itemId', (req: express.Request, res: express.Response) => {
+  const item = req.body as Item;
+
+  cart.remove(req.params.itemId);
+  const items = cart.getAll();
+
+  res.send(items);
+});
+
+// clear cart
+app.delete('/cart/items', (req: express.Request, res: express.Response) => {
+  const item = req.body as Item;
+
+  cart.clear();
+  const items = cart.getAll();
+
+  res.send(items);
 });
 
 app.listen(3000, () => {
